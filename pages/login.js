e489d867342da829f8007e0cc42e0d19a116f627
sss82941700 <sss82941700@gmail.com>
2025-08-06 22:25:17 +0800

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,12 @@
 export default function Login() {
   return (
     <main style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>會員登入 Login</h1>
-      <p>請輸入帳號與密碼。</p>
+      <h1>會員登入</h1>
       <form>
-        <input placeholder="帳號" /><br /><br />
+        <input type="text" placeholder="帳號" /><br /><br />
         <input type="password" placeholder="密碼" /><br /><br />
         <button type="submit">登入</button>
       </form>
     </main>
-  )
+  );
 }
