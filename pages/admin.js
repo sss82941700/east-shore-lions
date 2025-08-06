61745267db1bf3328b8c1915ec19eb30b2eee176
sss82941700 <sss82941700@gmail.com>
2025-08-06 22:25:42 +0800

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -2,7 +2,7 @@ export default function Admin() {
   return (
     <main style={{ padding: "2rem", fontFamily: "Arial" }}>
       <h1>管理後台 Admin Dashboard</h1>
-      <p>這是東岸獅子會的管理介面。</p>
+      <p>僅限管理員使用</p>
     </main>
   );
 }
