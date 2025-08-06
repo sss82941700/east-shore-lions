bfc89be144d1e5ab13ea53a7126dc5b1a22528e6
sss82941700 <sss82941700@gmail.com>
2025-08-06 20:55:48 +0800

    Delete pages directory

--- pages/pages/pages/admin.js
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>管理後台 Admin Dashboard</h1>
-      <p>這是東岸獅子會的管理介面。</p>
-    </main>
-  )
-}
